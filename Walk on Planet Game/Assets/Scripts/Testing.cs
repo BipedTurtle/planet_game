3807ca65a6f7089147cfeee12f8c3830f6b28a72
James Cho <chojames500@gmail.com>
2020-03-11 03:51:44 -0500

    Prototype models have been added

---
+++ Walk on Planet Game/Assets/Scripts/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vector = Vector3.up;
+        var worldVector = transform.TransformDirection(vector);
+        Debug.Log($"local: {vector}\nworld: {worldVector}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
