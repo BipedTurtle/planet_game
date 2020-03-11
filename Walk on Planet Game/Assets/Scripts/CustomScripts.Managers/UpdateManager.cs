82f3c3dc3582e82437e6324f710e78b7698cc748
James Cho <chojames500@gmail.com>
2020-03-11 03:56:37 -0500

    Update and Game managers scripts have been added

---
+++ Walk on Planet Game/Assets/Scripts/CustomScripts.Managers/UpdateManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace CustomScripts.Managers
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        #region Singleton
+        public static UpdateManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+        #endregion
+
+        public event Action GlobalUpdate;
+        public event Action FixedGlboalUpdate;
+
+        private void Update()
+        {
+            this.GlobalUpdate?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            this.FixedGlboalUpdate?.Invoke();
+        }
+    }
+}
